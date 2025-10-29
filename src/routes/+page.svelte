67ccdc226d72af5bbf0d5daca2dcba0fe38adc78
dedestem <david@davidnet.net>
2025-10-29 16:39:56 +0100

    Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -84,8 +84,8 @@
 {:else}
 	<div class="welcomebox">
 		<FlexWrapper width="100%" direction="row" justifycontent="space-around">
-			<h1 style="margin: 0px;">{greeting}</h1>
-			<FlexWrapper justifycontent="column" gap="var(--token-space-1)">
+			<h1 style="margin-top: 1rem;">{greeting}</h1>
+			<FlexWrapper justifycontent="column" gap="var(--token-space-2)">
 				<span>{time}</span>
 				<span><Weather /></span>
 			</FlexWrapper>

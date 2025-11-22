6ea955113101d7750d034bd4d5bd0014d0a3f597
dedestem <david@davidnet.net>
2025-11-22 17:01:25 +0100

    Styling

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -37,15 +37,29 @@
 	});
 </script>
 
-{#if error}
-	<Error pageName="Admin list" errorMSG="Unknown" />
-{:else}
-	<FlexWrapper width="100%" height="100%">
-		{#if Authenticated}
-			<LinkButton href="https://account.davidnet.net/admin/">Account Admin</LinkButton>
-			<LinkButton href="https://kanban.davidnet.net/admin/">Kanban Admin</LinkButton>
-		{:else}
-			<Loader />
-		{/if}
-	</FlexWrapper>
-{/if}
+<div id="background">
+	{#if error}
+		<Error pageName="Admin list" errorMSG="Unknown" />
+	{:else}
+		<FlexWrapper width="100%" height="100%">
+			{#if Authenticated}
+				<LinkButton href="https://account.davidnet.net/admin/">Account Admin</LinkButton>
+				<LinkButton href="https://kanban.davidnet.net/admin/">Kanban Admin</LinkButton>
+			{:else}
+				<Loader />
+			{/if}
+		</FlexWrapper>
+	{/if}
+</div>
+
+<style>
+	#background {
+		background-color: var(--token-color-surface-sunken-normal);
+		padding: 4rem 5rem;
+		padding-top: 0rem;
+		border-radius: 1rem;
+		width: 300px;
+		height: 680px;
+		box-shadow: 0 4px 12px rgba(0, 0, 0, 0.25);
+	}
+</style>

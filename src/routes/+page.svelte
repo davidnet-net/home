4be94cb41a7f09ad4482001907851505349d4c12
David <78974474+dedestem@users.noreply.github.com>
2025-12-09 09:16:07 +0000

    f

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -437,11 +437,11 @@
 <Space height="var(--token-space-4)" />
 
 <FlexWrapper alignitems="flex-start" width="80%">
-	<h2>Reccent activity:</h2>
+	<h2>Recent activity:</h2>
 	{#if boards_recent.length > 0}
 		<FlexWrapper gap="var(--token-space-3)" justifycontent={width > 600 ? "flex-start" : "space-evenly"} direction="row" wrap="wrap">
 			{#each boards_recent as board (board.id)}
-				<a class="option" href={"https://kanban.davidnet.net/board" + board.id} style="image-background: url('{board.background_url}'); background-size: cover; background-position: center;">
+				<a class="option" href={"https://kanban.davidnet.net/board" + board.id} style="background-image: url('{board.background_url}'); background-size: cover; background-position: center;">
 					<FlexWrapper width="100%" height="100%" gap="var(--token-space-2)">
 						<Icon size="4rem" icon="view_kanban" />
 						<p class="option-text">{board.name}</p>

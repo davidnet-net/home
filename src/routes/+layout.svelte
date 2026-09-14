89100f1156a9408d2f2d372b1826c52282391410
dedestem <david@davidnet.net>
2026-09-14 17:39:27 +0200

    Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
 	import { AppShell } from "@davidnet-net/svelte-ui";
-
 	import * as paraglideRuntime from "$lib/paraglide/runtime.js";
 
 	let { children } = $props();
+
+	const isHttps = typeof window !== "undefined" && window.location.protocol === "https:";
 </script>
+
+<!-- Als de site via HTTPS draait, dwingen we de browser om HTTP-requests te upgraden. Issue met wobble towers -->
+{#if isHttps}
+	<svelte:head>
+		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+	</svelte:head>
+{/if}
 
 <AppShell appName="Home" shortAppName="Home" {paraglideRuntime}>
 	{#snippet banners()}
